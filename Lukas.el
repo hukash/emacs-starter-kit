c91302eb5151b368973c02656eb4b1f7caff677e
lsobik <hukash@gmail.com>
2010-11-20 20:57:31 +0100

    changed theme to a light one

--- Lukas.el
+++ Lukas.el
@@ -4,10 +4,11 @@
 
 
 ;; Font
-(set-face-font 'default "-apple-menlo-medium-r-normal--12-0-72-72-m-0-iso10646-1")
+(set-face-font 'default "-apple-inconsolata-medium-r-normal--14-0-72-72-m-0-iso10646-1")
 
 ;; Color Themes
 (add-to-list 'load-path (concat dotfiles-dir "/vendor/color-theme"))
 (require 'color-theme)
 (color-theme-initialize)
-(color-theme-charcoal-black)
+(color-theme-standard)
+;;(color-theme-charcoal-black)
