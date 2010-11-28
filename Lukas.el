7d211f2bf19bec2cc0913be4472c039d3f0a905c
lsobik <hukash@gmail.com>
2010-11-28 19:58:25 +0100

    added monokai theme, changed cursor from block to bar

--- Lukas.el
+++ Lukas.el
@@ -2,13 +2,17 @@
 (require 'maxframe)
 (add-hook 'window-setup-hook 'maximize-frame t)
 
-
 ;; Font
-(set-face-font 'default "-apple-inconsolata-medium-r-normal--14-0-72-72-m-0-iso10646-1")
+(set-face-font 'default "-apple-inconsolata-medium-r-normal--16-0-72-72-m-0-iso10646-1")
 
 ;; Color Themes
 (add-to-list 'load-path (concat dotfiles-dir "/vendor/color-theme"))
 (require 'color-theme)
 (color-theme-initialize)
-(color-theme-standard)
-;;(color-theme-charcoal-black)
+;; (color-theme-zenburn)
+;; (color-theme-standard)
+;; (color-theme-charcoal-black)
+(color-theme-almost-monokai)
+
+;; Set cursor to bar
+(setq-default cursor-type 'bar)
